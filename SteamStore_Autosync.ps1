6fc5d5e5808d43579a46d23e126ff3769a86c7ad
Luis Antonio Garcia Roque <78177901+toonymak1993@users.noreply.github.com>
2024-03-25 15:18:39 +0100

    Add Powershell File
    First release. 
    Feature List: 
    1:Trigger SRM Parser
    2:Show Status in PS Console
    3:Restart Steam

---
+++ SteamStore_Autosync.ps1
@@ -0,0 +1,17 @@
+﻿
+Stop-Process -Name "Steam" -Force
+
+# wait
+do {
+    Start-Sleep -Seconds 1
+}
+while (Get-Process -Name "Steam" -ErrorAction SilentlyContinue)
+#change here the SRM EXE Link !!!!!!!!!!
+Start-Process "SRM LINK HERE.exe" -ArgumentList "add"
+# close Steam
+# wait 10 
+Start-Sleep -Seconds 5
+
+# Start Steam
+#change here the if needed !!!!!!!!!!
+Start-Process "C:\Program Files (x86)\Steam\steam.exe"
